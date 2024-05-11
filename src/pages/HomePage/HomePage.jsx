89f305b4ab3b3aa610d0bac4a1e780ab64f5c03a
MaiKimTuyet12062002 <131434981+MaiKimTuyet12062002@users.noreply.github.com>
2024-05-11 23:56:31 +0700

    cardProduct & Navbar

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import CardComponent from "../../Components/CardComponent/CardComponent";
+import NavBarComponent from "../../Components/NavbarComponent/NavBarComponent";
 
 const HomePage = () => {
-return (
-    <div>HomePage</div>
-)
+    return (
+        <>
+            <div id="container" style={{backgroundColor: '#efefef', padding: '0 120px', height: '1000px'}}>
+                <div style={{marginTop: '20px', display: 'flex', alignItems: 'center', gap: '20px'}}>
+                    <CardComponent/>
+                    <CardComponent/>
+                    <CardComponent/>
+                    <CardComponent/>
+                    <CardComponent/>
+                    <CardComponent/>
+                    <CardComponent/>
+                    <CardComponent/>
+                </div>
+                {/*<ButtonComponent textButton ="Xem thêm" type="outline" styleButton={{*/}
+                {/*    border: '1px solid rgb(11, 116, 229)', color: 'rgb(11, 116, 229)',*/}
+                {/*    width: '240px', height: '38px', borderRadius: '4px'*/}
+                {/*}}/>*/}
+            </div>
+        </>
+    )
 }
 
 export default HomePage
